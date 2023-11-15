c976ba6b341b16fbad571911f9721dd88d7834ba
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-11-15 14:00:22 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// ROUTERS
+// Middlewares
+import notFound from './middlewares/notFound.js';
+import errorHandler from './middlewares/errorhandler.js';
+// Access .env file
+dotenv.config();
+// Create HTTP Server
+const app = express();
+// Body Parser
+app.use(express.json());
+// API ROUTES
+// NOT FOUND Handler
+app.use(notFound);
+// Global Error Handler
+app.use(errorHandler);
+// Connect to Database and Start Listening
+try {
+  await mongoose.connect(process.env.MONGO_URL!);
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+} catch (error) {
+  console.log(error);
+  process.exit(0);
+}
